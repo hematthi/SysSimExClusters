56c380bf8a1d5eaaf0761a68ca3d47cfec23c733
Matthias Yang He <mhe@nd.edu>
2023-09-27 17:11:43 -0400

    Test and update scripts for optimizing hybrid model

--- src/optimize.jl
+++ src/optimize.jl
@@ -11,16 +11,15 @@ sim_param = setup_sim_param_model()
 
 ##### To start saving the model iterations in the optimization into a file:
 
-model_name = "Clustered_P_R_optimization"
-optimization_number = "_random"*ARGS[1] # if want to run on the cluster with random initial active parameters: "_random"*ARGS[1]
+model_name = "Hybrid1"
+run_number = ARGS[1] # if want to run on the cluster as part of a job array: ARGS[1]
 AD_mod = true
-num_targs = 79935
-max_incl_sys = 0.
-max_evals = 5000
-dists_include = ["delta_f", "mult_CRPD_r", "periods_KS", "period_ratios_KS", "durations_KS", "duration_ratios_nonmmr_KS", "duration_ratios_mmr_KS", "depths_KS", "radius_ratios_KS"]
+num_targs = 86760
+max_evals = 10 #5000
+dists_include = ["delta_f", "mult_CRPD_r", "periods_KS", "period_ratios_KS", "durations_KS", "duration_ratios_KS", "depths_KS", "radii_KS", "radius_ratios_KS", "radii_partitioning_KS", "radii_monotonicity_KS", "gap_complexity_KS"]
 Pop_per_param = 4
 
-file_name = model_name*optimization_number*"_targs$(num_targs)_evals$(max_evals).txt"
+file_name = model_name*"_targs$(num_targs)_evals$(max_evals)_run$(run_number).txt"
 f = open(file_name, "w")
 println(f, "# All initial parameters:")
 write_model_params(f, sim_param)
@@ -29,23 +28,18 @@ write_model_params(f, sim_param)
 
 
 
-##### To load a file with the weights, and simulate a reference catalog if we want to fit to a model:
+##### To load the Kepler catalog:
 
-# To generate a reference catalog, if we want to fit to a simulated catalog instead of data:
-#=
-Random.seed!(1234) # to have the same reference catalog
-cat_phys = generate_kepler_physical_catalog(sim_param)
-cat_phys_cut = ExoplanetsSysSim.generate_obs_targets(cat_phys,sim_param)
-cat_obs = observe_kepler_targets_single_obs(cat_phys_cut,sim_param)
-summary_stat_ref = calc_summary_stats_model(cat_obs,sim_param)
-=#
+ssk = calc_summary_stats_Kepler(stellar_catalog, planets_cleaned)
 
-# To simulate more observed planets for the subsequent model generations:
+##### To load a file with the weights:
+
+# To set the number of targets in the simulated catalog for each subsequent iteration:
 add_param_fixed(sim_param,"num_targets_sim_pass_one", num_targs)
-add_param_fixed(sim_param,"max_incl_sys", max_incl_sys)
 
 # To load and compute the weights, target distance, and target distance std from a precomputed file:
-active_param_true, weights, target_fitness, target_fitness_std = compute_weights_target_fitness_std_from_file("Clustered_P_R_weights_ADmod_$(AD_mod)_targs399675_evals1000.txt", 1000, sim_param; dists_include=dists_include, f=f)
+#active_param_true, weights, target_fitness, target_fitness_std = compute_weights_target_fitness_std_from_file("Maximum_AMD_model_split_stars_weights_ADmod_$(AD_mod)_targs86760_evals100_all_pairs.txt", 4950, sim_param; dists_include=dists_include, f=f)
+active_param_true, weights, target_fitness, target_fitness_std = compute_weights_target_fitness_std_from_file_split_samples("Maximum_AMD_model_split_stars_weights_ADmod_$(AD_mod)_targs86760_evals100_all_pairs.txt", 4950, sim_param; names_samples=String[], dists_include_samples=[String[]], dists_include_all=dists_include, f=f)
 
 
 
@@ -53,13 +47,12 @@ active_param_true, weights, target_fitness, target_fitness_std = compute_weights
 
 ##### To draw the initial values of the active parameters randomly within a search range:
 
-ParamsTriangles_all = [ExoplanetsSysSim.ParamsTriangle((9,10), (0.,0.), (30.,30.), (30.,0.))] #[]
-active_param_keys = ["f_high_incl", "f_stars_with_planets_attempted", "log_rate_clusters", "log_rate_planets_per_cluster", "power_law_P", "power_law_r1", "power_law_r2", "sigma_hk", "sigma_incl", "sigma_incl_near_mmr", "sigma_log_radius_in_cluster", "sigma_logperiod_per_pl_in_cluster"]
-active_params_box = [(0., 1.), (0., 1.), (log(0.5), log(5.)), (log(0.5), log(5.)), (-2., 2.), (-4., 2.), (-6., 0.), (0., 0.1), (0., 1.), (0., 1.), (0., 0.5), (0., 0.3)] #search ranges for all of the active parameters
+active_param_keys = ["break1_mass", "log_rate_clusters", "log_rate_planets_per_cluster", "mean_ln_mass", "norm_radius", "power_law_P", "power_law_γ0", "power_law_γ1", "power_law_σ0", "power_law_σ1", "sigma_ln_mass", "sigma_logperiod_per_pl_in_cluster"]
+active_params_box = [(1., 100.), (log(0.2), log(10.)), (log(0.2), log(10.)), (log(1.), log(100.)), (1., 5.), (-2., 2.), (0., 1.), (0., 1.), (0., 0.5), (0., 0.5), (log(1.), log(100.)), (0., 0.5)] #search ranges for all of the active parameters
 
 Random.seed!() # to have a random set of initial parameters and optimization run
 
-active_param_start, active_param_transformed_start = draw_random_active_params(active_param_keys, active_params_box, sim_param; PT_all=ParamsTriangles_all)
+active_param_start, active_param_transformed_start = draw_random_active_params(active_param_keys, active_params_box, sim_param)
 
 
 
@@ -69,11 +62,6 @@ println("# Active parameters: ", make_vector_of_active_param_keys(sim_param))
 println(f, "# Active parameters: ", make_vector_of_active_param_keys(sim_param))
 println(f, "# Starting active parameter values: ", active_param_start)
 println(f, "# Optimization active parameters search bounds: ", active_params_box)
-if length(ParamsTriangles_all) > 0
-    for pt in ParamsTriangles_all
-        println(f, "# Transformed active parameters: ", make_vector_of_active_param_keys(sim_param)[collect(pt.id_xy)], "; Triangle bounds: $(pt.A), $(pt.B), $(pt.C)")
-    end
-end
 println(f, "# Method: adaptive_de_rand_1_bin_radiuslimited")
 println(f, "# PopulationSize: ", PopSize)
 println(f, "# AD_mod: ", AD_mod)
@@ -86,8 +74,7 @@ println(f, "# Format: d_used_vals: [distance terms][sum of distance terms]")
 println(f, "# Format: d_used_vals_w: [weighted distance terms][sum of weighted distance terms]")
 println(f, "#")
 
-target_function(active_param_start, sim_param; ss_fit=ssk, dists_include=dists_include, weights=weights, AD_mod=AD_mod, f=f)
-target_function_transformed_params(active_param_transformed_start, ParamsTriangles_all, sim_param; ss_fit=ssk, dists_include=dists_include, weights=weights, AD_mod=AD_mod, f=f)
+target_function(active_param_start, sim_param; ss_fit=ssk, dists_include=dists_include, weights=weights["all"], AD_mod=AD_mod, f=f)
 
 
 
@@ -99,9 +86,7 @@ target_function_transformed_params(active_param_transformed_start, ParamsTriangl
 using BlackBoxOptim              # see https://github.com/robertfeldt/BlackBoxOptim.jl for documentation
 
 t_elapsed = @elapsed begin
-    #opt_result = bboptimize(active_params -> target_function(active_params, sim_param; ss_fit=ssk, dists_include=dists_include, weights=weights, AD_mod=AD_mod, f=f); SearchRange = active_params_box, NumDimensions = length(active_param_true), Method = :adaptive_de_rand_1_bin_radiuslimited, PopulationSize = PopSize, MaxFuncEvals = max_evals, TargetFitness = target_fitness, FitnessTolerance = target_fitness_std, TraceMode = :verbose)
-
-    opt_result = bboptimize(active_params -> target_function_transformed_params(active_params, ParamsTriangles_all, sim_param; ss_fit=ssk, dists_include=dists_include, weights=weights, AD_mod=AD_mod, f=f); SearchRange = active_params_box, NumDimensions = length(active_param_true), Method = :adaptive_de_rand_1_bin_radiuslimited, PopulationSize = PopSize, MaxFuncEvals = max_evals, TargetFitness = target_fitness, FitnessTolerance = target_fitness_std, TraceMode = :verbose)
+    opt_result = bboptimize(active_params -> target_function(active_params, sim_param; ss_fit=ssk, dists_include=dists_include, weights=weights["all"], AD_mod=AD_mod, f=f); SearchRange = active_params_box, NumDimensions = length(active_param_true), Method = :adaptive_de_rand_1_bin_radiuslimited, PopulationSize = PopSize, MaxFuncEvals = max_evals, TargetFitness = target_fitness, FitnessTolerance = target_fitness_std, TraceMode = :verbose)
 end
 
 println(f, "# best_candidate: ", best_candidate(opt_result))
--- src/optimize_split_stars.jl
+++ src/optimize_split_stars.jl
@@ -11,18 +11,17 @@ sim_param = setup_sim_param_model()
 
 ##### To start saving the model iterations in the optimization into a file:
 
-model_name = "H20"
-optimization_number = "_1" #"_random"*ARGS[1] # if want to run on the cluster with random initial active parameters: "_random"*ARGS[1]
+model_name = "Hybrid1"
+run_number = ARGS[1] # if want to run on the cluster as part of a job array: ARGS[1]
 names_split = ["bluer", "redder"]
 AD_mod = true
 num_targs = 86760
-max_incl_sys = 0.
 max_evals = 10 #5000
-dists_include_split = ["delta_f", "mult_CRPD_r", "periods_KS", "period_ratios_KS", "durations_KS", "duration_ratios_nonmmr_KS", "duration_ratios_mmr_KS", "depths_KS", "radius_ratios_KS"]
-dists_include_all = ["delta_f", "mult_CRPD_r", "periods_KS", "period_ratios_KS", "durations_KS", "duration_ratios_nonmmr_KS", "duration_ratios_mmr_KS", "depths_KS", "radius_ratios_KS"]
+dists_include_split = ["delta_f", "mult_CRPD_r", "periods_KS", "period_ratios_KS", "durations_KS", "duration_ratios_KS", "depths_KS", "radii_KS", "radius_ratios_KS", "radii_partitioning_KS", "radii_monotonicity_KS", "gap_complexity_KS"]
+dists_include_all = ["delta_f", "mult_CRPD_r", "periods_KS", "period_ratios_KS", "durations_KS", "duration_ratios_KS", "depths_KS", "radii_KS", "radius_ratios_KS", "radii_partitioning_KS", "radii_monotonicity_KS", "gap_complexity_KS"]
 Pop_per_param = 4
 
-file_name = model_name*optimization_number*"_targs$(num_targs)_evals$(max_evals).txt"
+file_name = model_name*"_targs$(num_targs)_evals$(max_evals)_run$(run_number).txt"
 f = open(file_name, "w")
 println(f, "# All initial parameters:")
 write_model_params(f, sim_param)
@@ -47,12 +46,11 @@ cssck = calc_summary_stats_collection_Kepler(stellar_catalog, planet_catalog, na
 
 ##### To load a file with the weights:
 
-# To simulate more observed planets for the subsequent model generations:
+# To set the number of targets in the simulated catalog for each subsequent iteration:
 add_param_fixed(sim_param,"num_targets_sim_pass_one", num_targs)
-add_param_fixed(sim_param,"max_incl_sys", max_incl_sys)
 
 # To load and compute the weights, target distance, and target distance std from a precomputed file:
-active_param_true, weights, target_fitness, target_fitness_std = compute_weights_target_fitness_std_from_file_split_samples("Clustered_P_R_split_stars_weights_ADmod_$(AD_mod)_targs88912_evals100_all_pairs.txt", 4950, sim_param; names_samples=names_split, dists_include_samples=[dists_include_split, dists_include_split], dists_include_all=dists_include_all, f=f)
+active_param_true, weights, target_fitness, target_fitness_std = compute_weights_target_fitness_std_from_file_split_samples("Maximum_AMD_model_split_stars_weights_ADmod_$(AD_mod)_targs86760_evals100_all_pairs.txt", 4950, sim_param; names_samples=names_split, dists_include_samples=[dists_include_split, dists_include_split], dists_include_all=dists_include_all, f=f)
 weights_split = [weights["bluer"], weights["redder"]]
 
 
@@ -61,13 +59,12 @@ weights_split = [weights["bluer"], weights["redder"]]
 
 ##### To draw the initial values of the active parameters randomly within a search range:
 
-ParamsTriangles_all = ExoplanetsSysSim.ParamsTriangle[] # [ExoplanetsSysSim.ParamsTriangle((9,10), (0.,0.), (90.,90.), (90.,0.))]
-active_param_keys = ["f_stars_with_planets_attempted_at_med_color", "f_stars_with_planets_attempted_color_slope", "log_rate_clusters", "log_rate_planets_per_cluster", "num_mutual_hill_radii", "power_law_P", "power_law_r1", "power_law_r2", "sigma_hk", "sigma_log_radius_in_cluster", "sigma_logperiod_per_pl_in_cluster"]
-active_params_box = [(0., 1.), (-1., 2.), (log(0.2), log(10.)), (log(0.2), log(10.)), (3., 20.), (-2., 2.), (-4., 2.), (-6., 0.), (0., 0.5), (0., 0.5), (0., 0.5)] #search ranges for all of the active parameters
+active_param_keys = ["break1_mass", "log_rate_clusters", "log_rate_planets_per_cluster", "mean_ln_mass", "norm_radius", "power_law_P", "power_law_γ0", "power_law_γ1", "power_law_σ0", "power_law_σ1", "sigma_ln_mass", "sigma_logperiod_per_pl_in_cluster"]
+active_params_box = [(1., 100.), (log(0.2), log(10.)), (log(0.2), log(10.)), (log(1.), log(100.)), (1., 5.), (-2., 2.), (0., 1.), (0., 1.), (0., 0.5), (0., 0.5), (log(1.), log(100.)), (0., 0.5)] #search ranges for all of the active parameters
 
 Random.seed!() # to have a random set of initial parameters and optimization run
 
-active_param_start, active_param_transformed_start = draw_random_active_params(active_param_keys, active_params_box, sim_param; PT_all=ParamsTriangles_all)
+active_param_start, active_param_transformed_start = draw_random_active_params(active_param_keys, active_params_box, sim_param)
 
 
 
@@ -77,11 +74,6 @@ println("# Active parameters: ", make_vector_of_active_param_keys(sim_param))
 println(f, "# Active parameters: ", make_vector_of_active_param_keys(sim_param))
 println(f, "# Starting active parameter values: ", active_param_start)
 println(f, "# Optimization active parameters search bounds: ", active_params_box)
-if length(ParamsTriangles_all) > 0
-    for pt in ParamsTriangles_all
-        println(f, "# Transformed active parameters: ", make_vector_of_active_param_keys(sim_param)[collect(pt.id_xy)], "; Triangle bounds: $(pt.A), $(pt.B), $(pt.C)")
-    end
-end
 println(f, "# Method: adaptive_de_rand_1_bin_radiuslimited")
 println(f, "# PopulationSize: ", PopSize)
 println(f, "# AD_mod: ", AD_mod)
@@ -96,7 +88,6 @@ println(f, "# Format: [sample] d_used_vals_w: [weighted distance terms][sum of w
 println(f, "#")
 
 target_function_split_stars(active_param_start, sim_param; cssc_fit=cssck, dists_include_all=dists_include_all, weights_all=weights["all"], names_samples=names_split, dists_include_samples=[dists_include_split, dists_include_split], weights_samples=weights_split, AD_mod=AD_mod, f=f)
-#target_function_transformed_params_split_stars(active_param_transformed_start, ParamsTriangles_all, sim_param; cssc_fit=cssck, dists_include_all=dists_include_all, weights_all=weights["all"], names_samples=names_split, dists_include_samples=[dists_include_split, dists_include_split], weights_samples=weights_split, AD_mod=AD_mod, f=f)
 
 
 
@@ -110,8 +101,6 @@ using BlackBoxOptim              # see https://github.com/robertfeldt/BlackBoxOp
 
 t_elapsed = @elapsed begin
     opt_result = bboptimize(active_params -> target_function_split_stars(active_params, sim_param; cssc_fit=cssck, dists_include_all=dists_include_all, weights_all=weights["all"], names_samples=names_split, dists_include_samples=[dists_include_split, dists_include_split], weights_samples=weights_split, AD_mod=AD_mod, f=f); SearchRange = active_params_box, NumDimensions = length(active_param_true), Method = :adaptive_de_rand_1_bin_radiuslimited, PopulationSize = PopSize, MaxFuncEvals = max_evals, TargetFitness = target_fitness, FitnessTolerance = target_fitness_std, TraceMode = :verbose)
-
-    #opt_result = bboptimize(active_params -> target_function_transformed_params_split_stars(active_params, ParamsTriangles_all, sim_param; cssc_fit=cssck, dists_include_all=dists_include_all, weights_all=weights["all"], names_samples=names_split, dists_include_samples=[dists_include_split, dists_include_split], weights_samples=weights_split, AD_mod=AD_mod, f=f); SearchRange = active_params_box, NumDimensions = length(active_param_true), Method = :adaptive_de_rand_1_bin_radiuslimited, PopulationSize = PopSize, MaxFuncEvals = max_evals, TargetFitness = target_fitness, FitnessTolerance = target_fitness_std, TraceMode = :verbose)
 end
 
 println(f, "# best_candidate: ", best_candidate(opt_result))
