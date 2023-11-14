466377a15cfae2a09eb87a76871c13e08068950e
Matthias Yang He <mhe@nd.edu>
2023-11-14 17:55:04 -0500

    Update script for recomputing distances for best models from optimization runs with split stars

--- src/compute_distances_given_params_split_stars.jl
+++ src/compute_distances_given_params_split_stars.jl
@@ -11,27 +11,25 @@ sim_param = setup_sim_param_model()
 
 ##### To start saving the model iterations in the optimization into a file:
 
-model_name = "Clustered_P_R"
+model_name = "Hybrid1"
 names_split = ["bluer", "redder"]
 AD_mod = true
 num_targs = 86760
-max_incl_sys = 0.
-dists_include_split = ["delta_f", "mult_CRPD_r", "periods_KS", "period_ratios_KS", "durations_KS", "duration_ratios_nonmmr_KS", "duration_ratios_mmr_KS", "depths_KS", "radius_ratios_KS"]
-dists_include_all = ["delta_f", "mult_CRPD_r", "periods_KS", "period_ratios_KS", "durations_KS", "duration_ratios_nonmmr_KS", "duration_ratios_mmr_KS", "depths_KS", "radius_ratios_KS"]
+dists_include_split = ["delta_f", "mult_CRPD_r", "periods_KS", "period_ratios_KS", "durations_KS", "duration_ratios_KS", "depths_KS", "radius_ratios_KS", "radii_partitioning_KS", "radii_monotonicity_KS", "gap_complexity_KS"]
+dists_include_all = ["delta_f", "mult_CRPD_r", "periods_KS", "period_ratios_KS", "durations_KS", "duration_ratios_KS", "depths_KS", "radius_ratios_KS", "radii_partitioning_KS", "radii_monotonicity_KS", "gap_complexity_KS"]
 
-data_table = CSV.read("../emulator/GP_files/durations_KS/Active_params_distances_table_best100000_every10.txt")
-n_params = length(make_vector_of_active_param_keys(sim_param))
-params_keys = names(data_table)[1:n_params]
+data_table = CSV.read("../emulator/GP_files/Active_params_distances_table_best100000_every10.txt", DataFrame)
+params_keys = names(data_table)[1:end-1]
 @assert all(make_vector_of_active_param_keys(sim_param) .== String.(params_keys))
 
-params_array = convert(Matrix, data_table[1:end, params_keys])
+params_array = Matrix(data_table[1:end, params_keys])
 
 run_number, runs = parse(Int64, ARGS[1]), parse(Int64, ARGS[2])
 evals = Int(size(params_array,1)/runs)
 start, stop = 1+(run_number-1)*evals, run_number*evals
 
 file_name = model_name*"_recompute_optim_best100000_every10_evals$(start)to$(stop)_targs$(num_targs).txt"
-f = open("durations_KS/"*file_name, "w")
+f = open(file_name, "w")
 println(f, "# All initial parameters:")
 write_model_params(f, sim_param)
 
@@ -55,12 +53,11 @@ cssck = calc_summary_stats_collection_Kepler(stellar_catalog, planet_catalog, na
 
 ##### To load a file with the weights:
 
-# To simulate more observed planets for the subsequent model generations:
+# To set the number of targets in the simulated catalog for each subsequent iteration:
 add_param_fixed(sim_param,"num_targets_sim_pass_one", num_targs)
-add_param_fixed(sim_param,"max_incl_sys", max_incl_sys)
 
 # To load and compute the weights, target distance, and target distance std from a precomputed file:
-active_param_true, weights, target_fitness, target_fitness_std = compute_weights_target_fitness_std_from_file_split_samples("Clustered_P_R_split_stars_weights_ADmod_$(AD_mod)_targs88912_evals100_all_pairs.txt", 4950, sim_param; names_samples=names_split, dists_include_samples=[dists_include_split, dists_include_split], dists_include_all=dists_include_all, f=f)
+active_param_true, weights, target_fitness, target_fitness_std = compute_weights_target_fitness_std_from_file_split_samples("Maximum_AMD_model_split_stars_weights_ADmod_$(AD_mod)_targs86760_evals100_all_pairs.txt", 4950, sim_param; names_samples=names_split, dists_include_samples=[dists_include_split, dists_include_split], dists_include_all=dists_include_all, f=f)
 weights_split = [weights["bluer"], weights["redder"]]
 
 
