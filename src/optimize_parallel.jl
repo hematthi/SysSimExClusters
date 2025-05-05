9023d0f74dbd1955ce198c8911e04a3b06c8f0e3
Matthias Yang He <mhe@nd.edu>
2025-05-05 16:32:55 -0700

    Remove big OSD_setup table on main processor to free up memory when running optimize_parallel.jl

--- src/optimize_parallel.jl
+++ src/optimize_parallel.jl
@@ -5,6 +5,10 @@ addprocs(4, exeflags="--project=$(Base.active_project())") # add number of addit
 
 @everywhere include("optimize.jl")
 
+# To remove the big OSD_setup table from the main processor to free up 8gb of memory:
+@eval ExoplanetsSysSim.WindowFunction OSD_setup = nothing
+GC.gc()
+
 run_ids = 1:25
 max_evals = 5000
 
