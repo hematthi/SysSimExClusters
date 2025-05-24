42ad33a86192d3fc7b2f890545e659f2c0e1a6bd
Matthias Yang He <mhe@nd.edu>
2025-05-24 13:48:46 -0700

    Simulate repeated catalogs from another set of model params that produces a strong radius valley

--- examples/generate_catalogs_repeated.jl
+++ examples/generate_catalogs_repeated.jl
@@ -9,7 +9,7 @@ include(joinpath(dir_path, "../src/planetary_catalog.jl"))
 
 ##### To set the active parameters for repeated catalog simulation:
 
-save_path = "/Users/hematthi/Documents/GradSchool/Research/SysSim/Simulated_catalogs/Hybrid_NR20_AMD_model1/Fit_some8_KS/Params9_fix_highM/Radius_valley_model62_repeated_100"
+save_path = "/Users/hematthi/Documents/GradSchool/Research/SysSim/Simulated_catalogs/Hybrid_NR20_AMD_model1/Fit_some8_KS/Params9_fix_highM/Radius_valley_model89_repeated_100"
 
 n_catalogs = 100 # number of catalogs to simulate
 
@@ -17,15 +17,15 @@ n_catalogs = 100 # number of catalogs to simulate
 sim_param = setup_sim_param_model()
 add_param_fixed(sim_param,"num_targets_sim_pass_one", 86760)
 
-add_param_active(sim_param, "log_rate_clusters", log(0.81))
-add_param_active(sim_param, "log_rate_planets_per_cluster", log(1.58))
-add_param_active(sim_param, "power_law_P", 0.24)
-add_param_active(sim_param, "mean_ln_mass", 0.94) # ln(Earth masses)
-add_param_active(sim_param, "sigma_ln_mass", 1.13) # ln(Earth masses)
+add_param_active(sim_param, "log_rate_clusters", log(0.32))
+add_param_active(sim_param, "log_rate_planets_per_cluster", log(3.11))
+add_param_active(sim_param, "power_law_P", 0.94)
+add_param_active(sim_param, "mean_ln_mass", 0.26) # ln(Earth masses)
+add_param_active(sim_param, "sigma_ln_mass", 1.92) # ln(Earth masses)
 add_param_active(sim_param, "norm_radius", 2.28) # Earth radii
-add_param_active(sim_param, "power_law_γ0", 0.06)
-add_param_active(sim_param, "power_law_σ0", 0.12)
-add_param_active(sim_param, "log_α_pret", log(2.71))
+add_param_active(sim_param, "power_law_γ0", 0.03)
+add_param_active(sim_param, "power_law_σ0", 0.10)
+add_param_active(sim_param, "log_α_pret", log(1.31))
 #####
 
 
