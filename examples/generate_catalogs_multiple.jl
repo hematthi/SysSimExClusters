e7358db91722767b2d1658c3c9bcd3283a1f4686
Matthias Yang He <mhe@nd.edu>
2025-08-08 16:27:29 -0700

    Update script for simulating multiple catalogs from an active params file

--- examples/generate_catalogs_multiple.jl
+++ examples/generate_catalogs_multiple.jl
@@ -12,11 +12,14 @@ add_param_fixed(sim_param,"num_targets_sim_pass_one", 86760)
 
 ##### To load a file with all the best active parameters from a set of optimization runs:
 
-files_directory = "Sims/"
-file_name = "Active_params_table.txt"
+save_path = "/Users/hematthi/Documents/GradSchool/Research/SysSim/Simulated_catalogs/Hybrid_NR20_AMD_model1/clustered_initial_masses/Fit_some8p1_KS/Params10_fix_highM/optim_best_100"
+file_path = "/Users/hematthi/Documents/NPP_ARC_Modernize_Kepler/Personal_research/SysSim/Model_Optimization/Hybrid_NR20_AMD_model1/clustered_initial_masses/Fit_some8p1_KS/Params10_fix_highM/best_N"
 
-active_params_best_all = CSV.read(files_directory*file_name, delim=" ")
-active_params_names = names(active_params_best_all)[2:end]
+file_name = "Active_params_table_best100.txt"
+
+optim_points = CSV.read(joinpath(file_path, file_name), DataFrame, delim=" ")
+active_params_names = names(optim_points)[2:end]
+active_params_best_all = optim_points[!,active_params_names]
 
 
 
@@ -24,15 +27,13 @@ active_params_names = names(active_params_best_all)[2:end]
 
 ##### To simulate a catalog for each set of best active parameters:
 
-for num_cat in 1:10 #1:size(active_params_best_all)[1]
-    println("Generating simulated catalog ", num_cat)
+for run_number in 1:size(active_params_best_all)[1]
+    println("Generating simulated catalog ", run_number)
 
     # To set up the model parameters:
     for (i,param_name) in enumerate(active_params_names)
-        add_param_active(sim_param, string(param_name), active_params_best_all[num_cat, param_name])
+        add_param_active(sim_param, string(param_name), active_params_best_all[run_number, param_name])
     end
 
-    run_number = active_params_best_all[num_cat, :run_number]
-
-    @time cat_phys, cat_phys_cut, cat_obs, summary_stat = generate_and_save_physical_and_observed_catalogs(sim_param; save_path=files_directory, run_number=run_number)
+    @time cat_phys, cat_phys_cut, cat_obs, summary_stat = generate_and_save_physical_and_observed_catalogs(sim_param; save_path=save_path, run_number=run_number)
 end
